9e129e040042be0d4895d90b3512a8bee11c1d2e
sharath <returncode13@gmail.com>
2023-04-21 13:53:04 +0530

    DDS router working inside k8

---
+++ record_microphone/microk8s/dds_router/dockerfiles/dds_router.Dockerfile
@@ -0,0 +1,4 @@
+##
+##This dockerfile uses the vulcanexus base image and copies the dds_route config file.
+##
+FROM --platform=amd eprosima/vulcanexus:humble-desktop
---
+++ record_microphone/microk8s/dds_router/router_configurations/change_router_ip.sh
@@ -0,0 +1,29 @@
+#https://stackoverflow.com/a/46645799/7835192
+
+#install jq usign apt-get install -y jq
+
+# This uses/requires the PyYAML library; "pip install PyYAML"
+yaml2json() {
+  /usr/bin/python3 -c 'import yaml, json, sys; print (json.dumps(yaml.safe_load(sys.stdin)))'
+}
+
+editYaml() {
+  local file=$1; shift
+  local tempfile=$(mktemp "${file}.XXXXXX")
+  local retval
+
+  if jq "$@" < <(yaml2json <"$file") >"$tempfile"; then
+    chmod --reference="$file" -- "$tempfile" # on GNU systems, preserve permissions
+    mv -- "$tempfile" "$file"
+  else
+    retval=$?
+    rm -f -- "$tempfile"
+    return "$retval"
+  fi
+}
+
+newDomain=$2
+newPort=$3
+
+editYaml $1 --arg newDomain "$newDomain" '.participants[1]."listening-addresses"[0].domain = $newDomain'
+editYaml $1 --arg newPort "$newPort" '.participants[1]."listening-addresses"[0].port = $newPort'
